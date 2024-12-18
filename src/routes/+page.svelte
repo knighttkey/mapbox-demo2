0e4cb1f36a188e0e05e38e73f3db2fed45de2e1f
kexy4958 <knighttkey@gmail.com>
2024-12-18 13:25:32 +0800

    navigation

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
+  onMount(() => {
+    goto("/cctvDemo6", { replaceState: true });
+  });
+</script>
+
+<style lang="postcss">
+</style>
